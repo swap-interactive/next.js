73f7f9d91e1248ce8ef02f21ab488c99e9f1526a
LongYinan <lynweklm@gmail.com>
2022-03-09 17:37:47 +0800

    Telemetry tracking event for emotion feature

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -1500,6 +1500,7 @@ export default async function getBaseWebpackConfig(
             ],
             ['swcRemoveConsole', !!config.compiler?.removeConsole],
             ['swcImportSource', !!jsConfig?.compilerOptions?.jsxImportSource],
+            ['swcEmotion', !!config.experimental.emotion],
           ])
         ),
     ].filter(Boolean as any as ExcludesFalse),
