481a4a2be20a82c58a7ef89d3a5245e4820dd34e
LongYinan <lynweklm@gmail.com>
2022-03-09 16:54:46 +0800

    Simply codes by reviewers suggestions, move emotion into experimental

--- packages/next/server/config-shared.ts
+++ packages/next/server/config-shared.ts
@@ -107,6 +107,13 @@ export interface ExperimentalConfig {
   outputFileTracingRoot?: string
   outputStandalone?: boolean
   middlewareSourceMaps?: boolean
+  emotion?:
+    | boolean
+    | {
+        sourceMap?: boolean
+        autoLabel?: 'dev-only' | 'always' | 'never'
+        labelFormat?: string
+      }
 }
 
 /**
@@ -382,13 +389,6 @@ export interface NextConfig extends Record<string, any> {
           exclude?: string[]
         }
     styledComponents?: boolean
-    emotion?:
-      | boolean
-      | {
-          sourceMap?: boolean
-          autoLabel?: 'dev-only' | 'always' | 'never'
-          labelFormat?: string
-        }
   }
 
   /**
